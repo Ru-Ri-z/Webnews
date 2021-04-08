83c90897d9dd6ba501e0803ac4e543597a755194
Ru-Ri-z <ivangorojovsky91gmail.com>
2021-04-08 14:01:42 -0300

    Update two

--- main.js
+++ main.js
@@ -4,5 +4,5 @@ document.querySelector('.menu-btn').addEventListener('click', () => {
 
 ScrollReveal().reveal('.showcase');
 ScrollReveal().reveal('.news-cards', { delay: 500});
-ScrollReveal().reveal('.news-cards', { delay: 500});
-ScrollReveal().reveal('.news-cards', { delay: 500});
+ScrollReveal().reveal('.cards-banner-one', { delay: 500});
+ScrollReveal().reveal('.cards-banner-two', { delay: 500});
